c51bbe928a383f1bba5039c7ba93d11a498340f2
Harith Bakri <harith@Hariths-iMac.local>
2014-06-12 19:45:24 +0800

    Initial Commit

---
+++ Calendario/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calendario
+//
+//  Created by Harith Bakri on 6/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calendario/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calendario
+//
+//  Created by Harith Bakri on 6/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
